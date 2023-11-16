a348db833b833218aa6136c99bb5becb9b9de7a7
Gabriel Marques <gabrielmrqs.contato@gmail.com>
2023-11-15 23:05:12 -0300

    configs refeitas

--- .config/nvim/README.md
+++ .config/nvim/README.md
@@ -1,1 +1,4 @@
-# Neovim configs using lazy plugin manager
+# 💤 LazyVim
+
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
